f537cb7b1c35711f9ab52dede47499cd66decee9
tutajrobert <tutajrobert@gmail.com>
2016-12-10 13:25:51 +0100

    Added material, repaired bmps and input.txt

--- mlib.py
+++ mlib.py
@@ -2,5 +2,6 @@ def mdict():
     #Material dictionary. m[mat name] = [young modulus, poisson ratio]
     m = {}
     m["steel"] = [205e9, 0.3]
-    m["alu"] = [8e10, 0.26]
+    m["alu"] = [80e9, 0.26]
+    m["titan"] = [110e9, 0.32]
     return m
