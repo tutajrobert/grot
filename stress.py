638b54d950fa49b647eb0f46dc0b638aae4e934d
tutaj robert <tutajrobert@gmail.com>
2019-01-17 13:23:06 +0100

    Backstress fighting

--- stress.py
+++ stress.py
@@ -82,15 +82,42 @@ def results(res_matrix, res_name, counter, E, v):
 
     elif res_name == "tot_strain":
         return [res_matrix[4][counter][2], "Total effective strain"]
-    
+ 
+    elif res_name == "plex":
+        return [res_matrix[4][counter][3], "plex"]
+        
+    elif res_name == "pley":
+        return [res_matrix[4][counter][4], "pley"]
+
+    elif res_name == "plexy":
+        return [res_matrix[4][counter][5], "plexy"]
+        
+    elif res_name == "plez":
+        return [res_matrix[4][counter][6], "plez"]
+ 
     elif res_name == "bstress":
         exx = res_matrix[0][counter][0] - res_matrix[4][counter][3]
         eyy = res_matrix[0][counter][1] - res_matrix[4][counter][4]
         exy = res_matrix[0][counter][2] - res_matrix[4][counter][5]
-        ezz = ((res_matrix[1][counter][0] + res_matrix[1][counter][1]) * -v / E) - res_matrix[4][counter][6]
+        #sxx = (E / (1 - (v**2))) * (exx + (v * eyy))
+        #syy = (E / (1 - (v**2))) * (eyy + (v * exx))
+        #ezz = ((sxx + syy) * -v / E) - res_matrix[4][counter][6]
+        #strains_t = [exx, eyy, exy]
+        #fc = E / (1 - (v ** 2))
+           
+        #slist = numpy.array(
+        #    [[fc, fc * v, 0],
+        #     [fc * v, fc, 0],
+        #     [0, 0, fc * ((1 - v)/ 2)]])
+                     
+        #s = (numpy.dot(slist, strains_t))
+        #ezz = ((s[0] + s[1]) * -v / E)# - res_matrix[4][counter][6]
+        #ezz = ((res_matrix[1][counter][0] + res_matrix[1][counter][1]) * -v / E) - res_matrix[4][counter][6]
+        v = .3
+        ezz = -((exx * v) + (eyy * v))
         eff_strain = (1 / (math.sqrt(2) * (1 + v))) * math.sqrt(((exx - eyy)**2 + (eyy - ezz)**2 + (ezz - exx)**2) + ((3/2)*(exy**2)))
         #plstrain = res_matrix[4][counter][0]
-        value = eff_strain
+        value = 205e3 * eff_strain
         return [value, "Back stress"]
     else:
         pass
