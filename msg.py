0a330ed0a83ed38d11a3c2c6930c11e4f2bcbe4d
tutaj robert <tutajrobert@gmail.com>
2019-04-02 13:55:51 +0200

    Code cleaning and tweaking no 3a

--- msg.py
+++
@@ -1,9 +0,0 @@
-import version
-
-vers = version.get()
-
-def welcome():
-    #Welcome message
-    print("")
-    print("GRoT> ver. " + vers + ", [Graficzny Rozwiązywacz Tarcz]")
-    print("................................................\n")
